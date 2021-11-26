07d5d7234257b862bc17e3f741cf5f6f533a2374
Jan Pierre <gamunaxx@gmail.com>
2021-11-26 18:25:59 -0500

    feat: create new user

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,11 +1,23 @@
 const Usuario = require('../models/Usuario');
+const bcryptjs = require('bcryptjs');
 
 exports.createUser = async (req, res) => {
+
+  const { email, password } = req.body;
+
   try {
-    const usuario = new Usuario(req.body);
+    let usuario = await Usuario.findOne({ email });
+    if (usuario) {
+      return res.status(400).json({ msg: 'El usuario ya existe' });
+    }
+
+    usuario = new Usuario(req.body);
+
+    const salt = await bcryptjs.genSalt(10);
+    usuario.password = await bcryptjs.hash(password, salt);
     await usuario.save();
 
-    res.send('Usuario creado correctamente');
+    res.status(200).json({ msg: 'Usuario creado correctamente' });
   } catch (error) {
     console.log('error', error);
     res.status(400).send('Ocurrió un error');
