89e4a1f3d03655771d55d6b3ccde66427d5e55c3
Jan Pierre <gamunaxx@gmail.com>
2021-11-26 18:54:34 -0500

    feat: add validator and jwt

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,23 +1,44 @@
 const Usuario = require('../models/Usuario');
 const bcryptjs = require('bcryptjs');
+const { validationResult } = require('express-validator');
+
+const jwt = require('jsonwebtoken');
 
 exports.createUser = async (req, res) => {
+
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
 
   const { email, password } = req.body;
 
   try {
-    let usuario = await Usuario.findOne({ email });
-    if (usuario) {
+    let user = await Usuario.findOne({ email });
+    if (user) {
       return res.status(400).json({ msg: 'El usuario ya existe' });
     }
 
-    usuario = new Usuario(req.body);
+    user = new Usuario(req.body);
 
     const salt = await bcryptjs.genSalt(10);
-    usuario.password = await bcryptjs.hash(password, salt);
-    await usuario.save();
+    user.password = await bcryptjs.hash(password, salt);
+    await user.save();
 
-    res.status(200).json({ msg: 'Usuario creado correctamente' });
+    const payload = {
+      user: {
+        id: user.id
+      }
+    };
+
+    jwt.sign(payload, process.env.SECRET_KEY, {
+      expiresIn: 3_600
+    }, (error, token) => {
+      if (error) throw error;
+
+      res.status(200).json({ token });
+    });
+
   } catch (error) {
     console.log('error', error);
     res.status(400).send('Ocurrió un error');
--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const usuarioController = require('../controllers/usuarioController');
+const { check } = require('express-validator');
 
 // * api/usuarios
-router.post('/', 
- usuarioController.createUser
+router.post('/',
+  [
+    check('name', 'name is required').not().isEmpty(),
+    check('email', 'add to email valid').isEmail(),
+    check('password', 'password must be minimum of 6 caracters').isLength({ min: 6 })
+  ],
+  usuarioController.createUser
 );
 
 module.exports = router;
